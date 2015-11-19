1b6a0f200205f3e793ca110cd06b3c3902532479
Ryan Magani <rmagani@cs.washington.edu>
2015-11-18 19:22:23 -0800

    basic 1:1 file serving

--- server.go
+++ server.go
@@ -8,10 +8,22 @@ import (
 
 func main() {
 	fmt.Printf("Hello World\n")
-	http.HandleFunc("/", game)
-	http.ListenAndServe(":7777", nil)
+	// http.Handle("/", http.FileServer(http.Dir("/site")))
+	// http.HandleFunc("/", game)
+	// http.HandleFunc("/draw", draw)
+	// http.HandleFunc("/guess", guess)
+	http.ListenAndServe(":7777", http.FileServer(http.Dir("./public")))
 }
 
-func game(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintf(w, "Welcome to the page")
-}
+// func game(w http.ResponseWriter, r * http.Request) {
+// 	// fmt.Fprintf(w, "Welcome to the page")
+// 	// http.ServeFile(w, r, r.URL.Path)
+	// http.FileServer(http.Dir("/site"))
+	// http.ServeFile(w, r, "css/compressed.css")
+// }
+
+// func draw(w http.ResponseWriter, r * http.Request) {
+// }
+
+// func guess(w http.ResponseWriter, r * http.Request) {
+// }
