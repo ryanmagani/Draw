4392b0b6bfd0f7286aaa19fdbcd194a33f130a10
Ryan Magani <rmagani@cs.washington.edu>
2015-11-18 19:32:44 -0800

    server now 1:1s public folder but also accepts request to /draw

--- server.go
+++ server.go
@@ -8,22 +8,14 @@ import (
 
 func main() {
 	fmt.Printf("Hello World\n")
-	// http.Handle("/", http.FileServer(http.Dir("/site")))
-	// http.HandleFunc("/", game)
-	// http.HandleFunc("/draw", draw)
-	// http.HandleFunc("/guess", guess)
-	http.ListenAndServe(":7777", http.FileServer(http.Dir("./public")))
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/draw", draw)
+	http.ListenAndServe(":7777", nil)
 }
 
-// func game(w http.ResponseWriter, r * http.Request) {
-// 	// fmt.Fprintf(w, "Welcome to the page")
-// 	// http.ServeFile(w, r, r.URL.Path)
-	// http.FileServer(http.Dir("/site"))
-	// http.ServeFile(w, r, "css/compressed.css")
-// }
-
-// func draw(w http.ResponseWriter, r * http.Request) {
-// }
+func draw(w http.ResponseWriter, r * http.Request) {
+	fmt.Printf("draw recieved")
+}
 
 // func guess(w http.ResponseWriter, r * http.Request) {
 // }
